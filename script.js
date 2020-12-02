ab77111fc936b6ed42a4db8cf232df358772bad6
Sandeep <sandeep98a@gmail.com>
2020-12-03 01:35:12 +0530

    style: pages styled

---
+++ script.js
@@ -0,0 +1,29 @@
+var translateBtn = document.querySelector("#translate-btn");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("error occured", error)
+    alert("Something wrong with server! try again after some time")
+}
+
+translateBtn.addEventListener("click", function clickEventHandler(){
+
+    var inputText =txtInput.value; //taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText// output
+    })
+    .catch(errorHandler)
+})
+
